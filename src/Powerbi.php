8655dd2ecee898b96a57bb02d8317f8fdaacca7a
DEV <dev@aspire>
2016-12-13 11:47:23 -0600

    version 0.0.1

---
+++ src/Powerbi.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Helper para formatear en json paginador
+ * 
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @author Ing. Eduardo Ortiz
+ * 
+ */
+namespace Adteam\Core\Powerbi;
+
+class Powerbi{ 
+}
